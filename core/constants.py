c42dfcc847c043c3049dcb16b77e6f945e8776a6
Guillermo Eijo <guillermoeijo@gmail.com>
2021-02-19 04:11:14 -0300

    Style fixes

---
+++ core/constants.py
@@ -0,0 +1,2 @@
+ALREADY_USED_MESSAGE = 'Hash already in use.'
+NOT_FOUND_MESSAGE = 'URL Not Found. Check your code or create a new one.'
--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
+from django.contrib import messages
+from django.urls import reverse
 from django.views import generic, View
 from django.shortcuts import render, redirect
 from django.core.exceptions import MultipleObjectsReturned
 
 from core.models import Url
 from core.forms import UrlForm
+from core.constants import ALREADY_USED_MESSAGE, NOT_FOUND_MESSAGE
 
 
 class HomeView(View):
@@ -21,7 +24,8 @@ class HomeView(View):
             if hashed_url:
                 obj = Url.objects.filter(hashed_url=hashed_url).first()
                 if obj is not None and obj.url != url:
-                    context = {'error': 'Hash already in use'}
+                    context = {"form": form}
+                    messages.error(request, ALREADY_USED_MESSAGE)
                 elif obj is not None and obj.url == url:
                     context = {'short_url': obj.get_full_short_url()}
                 else:
@@ -43,14 +47,13 @@ class HomeView(View):
 
 
 class RedirectView(View):
-    template_name = 'home.html'
 
     def get(self, request, *args, **kwargs):
         hashed_url = kwargs.get('hash')
         try:
             obj = Url.objects.get(hashed_url=hashed_url)
         except Url.DoesNotExist:
-            context = {'error': 'Not found'}
-            return render(request, self.template_name, context)
+            messages.error(request, NOT_FOUND_MESSAGE)
+            return redirect(reverse('home_page'))
 
         return redirect(obj.url)
