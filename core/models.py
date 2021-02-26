e1211ebaea9b26c9a0713395a408055ed9928e06
Guillermo Eijo <guillermoeijo@gmail.com>
2021-02-25 21:17:13 -0300

    Use random token instead of hash relative to url string

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
-import hashlib
+import secrets
+
 from django.db import models
 from django.conf import settings
 
@@ -12,11 +13,14 @@ class Url(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.hashed_url:
-            self.hashed_url = self.hash_url(self.url)
+            self.hashed_url = self.hash_url()
         super().save(*args, **kwargs)
 
-    def hash_url(self, url):
-        return hashlib.sha1(url.encode("utf-8")).hexdigest()[:10]
+    def hash_url(self):
+        token = secrets.token_urlsafe(16)[:10]
+        if Url.objects.filter(hashed_url=token):
+            token = self.hash_url()
+        return token
 
     def get_full_short_url(self):
         return f'{settings.BASE_URL}{self.hashed_url}'
